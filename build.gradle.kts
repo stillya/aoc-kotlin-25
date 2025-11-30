bab68f7fa0caa3cd5f69cb1a7a98e4bf303e452d
stillya <st.ilya.101@gmail.com>
2025-11-30 21:52:26 +0500

    final preparation for day01

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
+	kotlin("jvm") version "2.2.21"
+}
+
+dependencies {
+	implementation("io.github.dmitrynekrasov:kodvent:0.1.8")
 }
 
 sourceSets {
-    main {
-        kotlin.srcDir("src")
-    }
+	main {
+		kotlin.srcDir("src")
+	}
 }
 
 tasks {
-    wrapper {
-        gradleVersion = "9.2.1"
-    }
+	wrapper {
+		gradleVersion = "9.2.1"
+	}
 }
