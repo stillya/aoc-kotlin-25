6a86dd53db5e4bec59f3f5192b3c9c3f09439e76
stillya <st.ilya.101@gmail.com>
2025-12-02 04:00:37 +0500

    complete 1st day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,69 @@
 fun main() {
 	fun part1(input: List<String>): Int {
-		return input.size
+		val ring = ModRing(100) // 0 to 99
+		var point = 50
+		var zeroPoint = 0
+		input.map {
+			it[0] to it.drop(1).toInt()
+		}.forEach { (dir, value) ->
+			when (dir) {
+				'L' -> {
+					point = ring.minus(point, value)
+				}
+
+				'R' -> {
+					point = ring.add(point, value)
+				}
+			}
+			if (point == 0) {
+				zeroPoint++
+			}
+		}
+
+		return zeroPoint
 	}
 
 	fun part2(input: List<String>): Int {
-		return input.size
+		val ring = ModRing(100) // 0 to 99
+		var point = 50
+		var wraparounds = 0
+		input.map {
+			it[0] to it.drop(1).toInt()
+		}.forEach { (dir, value) ->
+			when (dir) {
+				'L' -> {
+					when {
+						point == 0 -> wraparounds += value / 100
+						point <= value -> wraparounds += 1 + (value - point) / 100
+					}
+					point = ring.minus(point, value)
+				}
+
+				'R' -> {
+					wraparounds += (point + value) / 100
+					point = ring.add(point, value)
+				}
+			}
+		}
+
+		return wraparounds
+
 	}
 
-	// Test if implementation meets criteria from the description, like:
-	check(part1(listOf("test_input")) == 1)
-
-	// Or read a large test input from the `src/Day01_test.txt` file:
-	val testInput = readInput("Day01_test")
-	check(part1(testInput) == 1)
-
-	// Read the input from the `src/Day01.txt` file.
 	val input = readInput("Day01")
+
 	part1(input).println()
 	part2(input).println()
+}
+
+class ModRing(private val n: Int) {
+
+	private fun norm(x: Int): Int {
+		return ((x % n) + n) % n
+	}
+
+	fun add(a: Int, b: Int): Int = norm(a + b)
+
+	fun minus(a: Int, b: Int): Int = norm(a - b)
+
 }
